60822de369ec85dc0538d5d39dd46f6d6be7a494
Pj <petejw@theswamp.co.uk>
2022-03-12 16:14:38 +0000

    Add project files.

---
+++ GetDateRanges/DateRange.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GetDateRanges
+{
+    public class DateRange
+    {
+        public DateRange(string start, string end)
+            : this(DateTime.Parse(start), DateTime.Parse(end))
+        {
+        }
+
+        public DateRange(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Start} - {End}";
+        }
+    }
+
+
+    /// <summary>
+    /// Some helper methods
+    /// </summary>
+    public static class DateRangeExtensions
+    {
+        public static IEnumerable<DateRange> ConsolidateRange(this IEnumerable<DateRange> source)
+        {
+            var periods = new List<DateRange>();
+            var dates = source.ToList(); // Take a copy as we mutate it (we remove ranges as we process)
+
+            // Start with earliest start
+            var range = dates.Next(DateTime.MinValue);
+
+            while (range != null)
+            {
+                // Find a range that overlaps the current range. Get the one with the latest end date
+                var overlappingRange = dates.LatestOverlappingByEndDate(range);
+
+                // Didn't find one. End of range, store it and look for next range
+                if (overlappingRange == null)
+                {
+                    periods.Add(range);
+                    range = dates.Next(range.End);
+                }
+
+                // Found one: Expand our range to cover it.
+                else
+                {
+                    // Remove this range from the source
+                    dates.Remove(overlappingRange);
+
+                    range.End = overlappingRange.End;
+                }
+            }
+
+            return periods;
+        }
+
+
+        // Find any ovelapping
+        // Get the source range that has:
+        //  a start between currant dt start/end
+        //  an end later than currant dt end
+        private static DateRange LatestOverlappingByEndDate(this IEnumerable<DateRange> source, DateRange range)
+        {
+            return source
+                .OrderByDescending(d => d.End)
+                .FirstOrDefault(d =>
+                    d != range
+                    && d.Start >= range.Start
+                    && d.Start <= range.End
+                    && d.End > range.End);
+        }
+
+
+        /// <summary>
+        /// Next range with a start greater than minStart
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="minStart"></param>
+        /// <returns></returns>
+        private static DateRange Next(this IEnumerable<DateRange> source, DateTime minStart)
+        {
+            return source
+                .Where(d => d.Start > minStart)
+                .OrderBy(d => d.Start)
+                .FirstOrDefault();
+        }
+    }
+}
---
+++ GetDateRanges/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GetDateRanges
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var dates = Mock();
+            dates.Dump("Source data");
+
+            dates = dates.ConsolidateRange();
+
+            Console.WriteLine();
+            dates.Dump("Periods");
+        }
+
+
+        static void Dump(this IEnumerable<DateRange> source, string title = null)
+        {
+            if(title != null)
+            {
+                Console.WriteLine(title);
+            }
+
+            foreach (var r in source.OrderBy(x => x.Start))
+            {
+                Console.WriteLine(r);
+            }
+        }
+
+
+        static IEnumerable<DateRange> Mock()
+        {
+            return new[] {
+                new DateRange("01 feb 2022 10:00", "02 feb 2022 15:00"),
+
+                new DateRange("03 feb 2022 10:00", "10 feb 2022 15:00"),
+                new DateRange("05 feb 2022 18:00", "06 feb 2022 09:00"),
+                new DateRange("09 feb 2022 10:00", "15 feb 2022 19:00"),
+
+                // Couple of dups
+                new DateRange("09 feb 2022 10:00", "15 feb 2022 19:00"),
+                new DateRange("09 feb 2022 10:00", "15 feb 2022 19:00"),
+
+                new DateRange("20 feb 2022 08:00", "23 feb 2022 11:00"),
+                new DateRange("21 feb 2022 08:00", "25 feb 2022 20:00")
+            };
+        }
+    }
+}
